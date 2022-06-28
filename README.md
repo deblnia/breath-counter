7dfc999acca4bff88e2afff2f2720fcf3a79e7c7
Deblina Mukherjee <deblinam@fb.com>
2022-06-27 23:27:25 -0400

    fixed the timing but the animation is still kinda janky

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ TODO:
 References:
 
 - [Relaxer App](https://github.com/bradtraversy/vanillawebprojects/tree/master/relaxer-app)
+- [4-7-8](https://github.com/djru/478)
--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 const container = document.getElementById("container");
 const text = document.getElementById("text");
 
-const totalTime = 9500;
-const breatheTime = (totalTime / 5) * 2;
-const holdTime = totalTime / 5;
+const [i, h, o] = [4000, 7000, 8000]
+const total = i + h + o
 
-breathAnimation();
+const start = Date.now()
+setInterval(() => {
+  let elapsed = (Date.now() - start) % total
+  if (elapsed < i) {
+    text.innerText = 'Breathe In'
+    container.className = 'container grow'
+  } else if (elapsed > i + h) {
+    text.innerText = 'Breathe Out'
+    container.className = 'container shrink'
+  } else if ((elapsed < i + h) & (elapsed > i)) {
+    text.innerText = "Hold"
+  }
+}, 10)
 
-function breathAnimation() {
-  text.innerText = "Breathe In!";
-  container.className = "container grow";
-  setTimeout(() => {
-    text.innerText = "Hold";
-    setTimeout(() => {
-      text.innerText = "Breathe Out!";
-      container.className = "container shrink";
-    }, holdTime);
-  }, breatheTime);
-}
+let rotations = 0;
 
-setInterval(breathAnimation, totalTime);
-
-rotations = 0;
 function increment_rotations() {
   rotations += 1;
   document.getElementById("outer text").textContent = String(rotations);
 }
 
-setInterval(increment_rotations, breatheTime);
+setInterval(increment_rotations, total);
